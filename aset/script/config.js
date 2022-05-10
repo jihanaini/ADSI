b2a00cd683a8275bf35006ad905e16812dbcc4fe
unknown <dewiayupurnama333@gmail.com>
2022-05-10 16:52:56 +0700

    add config

---
+++ aset/script/config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#FFC600',
+            },
+            fontFamily: {
+                'poppins': ['Poppins', 'sans-serif'],
+                'halant': ['Halant', 'serif']
+            },
+        }
+    }
+}
