de581c87351531f9441103597ff6d7d49477f6f5
auryhs <arumabrori7@gmail.com>
2022-05-11 02:46:19 +0700

    feat: add dp, dp2, checkout

--- aset/script/config.js
+++ aset/script/config.js
@@ -3,11 +3,20 @@ tailwind.config = {
         extend: {
             colors: {
                 primary: '#FFC600',
+                green1: '#CCFFED',
+                green2: '#62FEC5',
+                oren: '#F1571C',
+                latar: '#F4F4F4'
             },
             fontFamily: {
                 'poppins': ['Poppins', 'sans-serif'],
                 'halant': ['Halant', 'serif']
             },
+            boxShadow:{
+                'all' : '-5px 1px 8px 2px #f4f4'
+                // 0 35px 60px -15px rgba(0, 0, 0, 0.3)
+                // 1px 0 7px -2px #888
+            }
         }
     }
 }
